227a9ed0a87bc69b072e070a6ee1ff8c7046ee25
Hussien-3s <hussienksab7@gmail.com>
2026-05-14 15:52:31 +0300

    Refactor API calls to use axiosConfig

--- frontend/app/actions/getFreindId.ts
+++ frontend/app/actions/getFreindId.ts
@@ -1,11 +1,11 @@
-import axios from "axios"
+import API from "@/config/axiosConfig"
 
 export async function FreindId(username: string) {
-    const res = await axios.get(`http://localhost:8080/api/get-username/${username}`)
+    const res = await API.get(`/get-username/${username}`)
     return res.data
 }
 
 export async function FreindName(Id: string) {
-    const res = await axios.get(`http://localhost:8080/api/get-user-by-id/${Id}`)
+    const res = await API.get(`/get-user-by-id/${Id}`)
     return res.data
 }
